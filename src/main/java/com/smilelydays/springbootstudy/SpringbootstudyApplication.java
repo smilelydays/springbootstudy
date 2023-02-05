5083eb9c3e38177a850f32c68d2a7e71451e0a7f
smilelydays <smilelygirl@naver.com>
2023-02-05 11:31:58 +0900

    Create Web Server (Tomcat)

--- src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
@@ -1,9 +1,14 @@
 package com.smilelydays.springbootstudy;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class SpringbootstudyApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
-
 }
