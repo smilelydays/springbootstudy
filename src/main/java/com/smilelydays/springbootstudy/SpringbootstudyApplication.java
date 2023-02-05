1b1a1a449c3bd503574fb668bb7b6152ce9ef764
smilelydays <smilelygirl@naver.com>
2023-02-05 12:01:19 +0900

    Register bean of HelloController

--- src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
@@ -7,19 +7,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 
 import java.io.IOException;
 
 public class SpringbootstudyApplication {
 
     public static void main(String[] args) {
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
 
-		HelloController helloController = new HelloController();
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
 		WebServer webServer = serverFactory.getWebServer(servletContext ->
 				servletContext
@@ -28,10 +30,12 @@ public class SpringbootstudyApplication {
 							protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 								if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 									String name = req.getParameter("name");
+
+									HelloController helloController = applicationContext.getBean(HelloController.class);
 									String ret = helloController.hello(name);
 
 									resp.setStatus(HttpStatus.OK.value());
-									resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+									resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 									resp.getWriter().println(ret);
 								} else {
 									resp.setStatus(HttpStatus.NOT_FOUND.value());
