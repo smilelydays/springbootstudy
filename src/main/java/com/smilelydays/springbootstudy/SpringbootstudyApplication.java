9fdf44e4c05b2a3540287125622bee9dd7e619df
smilelydays <smilelygirl@naver.com>
2023-02-05 11:42:20 +0900

    Refactoring

--- src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
@@ -18,8 +21,8 @@ public class SpringbootstudyApplication {
 						.addServlet("hello", new HttpServlet() {
 							@Override
 							protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-								resp.setStatus(200);
-								resp.setHeader("Content-Type", "text/plain");
+								resp.setStatus(HttpStatus.OK.value());
+								resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
 								resp.getWriter().println("Hello Servlet");
 							}
 						})
