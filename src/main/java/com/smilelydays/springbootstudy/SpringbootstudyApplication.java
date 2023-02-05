85e3e98a517a18512de09185cd810cc41b51861b
smilelydays <smilelygirl@naver.com>
2023-02-05 11:48:37 +0900

    Introduce a frontController

--- src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,17 +19,21 @@ public class SpringbootstudyApplication {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext ->
 				servletContext
-						.addServlet("hello", new HttpServlet() {
+						.addServlet("frontController", new HttpServlet() {
 							@Override
 							protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-								String name = req.getParameter("name");
+								if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+									String name = req.getParameter("name");
 
-								resp.setStatus(HttpStatus.OK.value());
-								resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-								resp.getWriter().println("Hello " + name);
+									resp.setStatus(HttpStatus.OK.value());
+									resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+									resp.getWriter().println("Hello " + name);
+								} else {
+									resp.setStatus(HttpStatus.NOT_FOUND.value());
+								}
 							}
 						})
-						.addMapping("/hello")
+						.addMapping("/*")
 		);
         webServer.start();
     }
