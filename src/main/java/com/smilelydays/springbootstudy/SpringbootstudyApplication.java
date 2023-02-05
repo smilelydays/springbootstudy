6c83f5342a1b9f50f3d17b9560f73cc353b2cc09
smilelydays <smilelygirl@naver.com>
2023-02-05 11:53:37 +0900

    Move biz logic to helloController

--- src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
@@ -1,5 +1,6 @@
 package com.smilelydays.springbootstudy;
 
+import com.smilelydays.springbootstudy.controller.HelloController;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,6 +18,9 @@ public class SpringbootstudyApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+		HelloController helloController = new HelloController();
+
 		WebServer webServer = serverFactory.getWebServer(servletContext ->
 				servletContext
 						.addServlet("frontController", new HttpServlet() {
@@ -24,10 +28,11 @@ public class SpringbootstudyApplication {
 							protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 								if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 									String name = req.getParameter("name");
+									String ret = helloController.hello(name);
 
 									resp.setStatus(HttpStatus.OK.value());
 									resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-									resp.getWriter().println("Hello " + name);
+									resp.getWriter().println(ret);
 								} else {
 									resp.setStatus(HttpStatus.NOT_FOUND.value());
 								}
---
+++ src/main/java/com/smilelydays/springbootstudy/controller/HelloController.java
@@ -0,0 +1,7 @@
+package com.smilelydays.springbootstudy.controller;
+
+public class HelloController {
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
