656e12a916bafa52860772da384bc1aedac03bcd
smilelydays <smilelygirl@naver.com>
2023-02-05 15:02:55 +0900

    Register beans - servletWebServerFactory, dispatcherServlet

--- src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/smilelydays/springbootstudy/SpringbootstudyApplication.java
@@ -3,6 +3,7 @@ package com.smilelydays.springbootstudy;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,23 +13,36 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 public class SpringbootstudyApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
 
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//				dispatcherServlet.setApplicationContext(this); //Bean 생성 시에 셋팅되기 때문에 생략 가능 (ApplicationContextAware 참고)
 
-				WebServer webServer = serverFactory.getWebServer(servletContext ->
-						servletContext
-								.addServlet("dispatcherServlet", new DispatcherServlet(this))
-								.addMapping("/*")
-				);
-				webServer.start();
-			}
-		};
-		applicationContext.register(SpringbootstudyApplication.class);
-		applicationContext.refresh();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                            servletContext
+                                    .addServlet("dispatcherServlet", dispatcherServlet)
+                                    .addMapping("/*");
+                        }
+                );
+                webServer.start();
+            }
+        };
+        applicationContext.register(SpringbootstudyApplication.class);
+        applicationContext.refresh();
     }
 }
