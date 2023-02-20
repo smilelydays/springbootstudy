386eeeec328b118a7eafc5df3726ea6c158cd547
smilelydays <smilelygirl@naver.com>
2023-02-20 21:23:26 +0900

    Create @UnitTest

--- src/test/java/com/smilelydays/springbootstudy/service/SimpleHelloServiceTest.java
+++ src/test/java/com/smilelydays/springbootstudy/service/SimpleHelloServiceTest.java
@@ -2,10 +2,21 @@ package com.smilelydays.springbootstudy.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@interface UnitTest {
+}
+
 class SimpleHelloServiceTest {
-    @Test
+    @UnitTest
     void say_hello_test() {
         SimpleHelloService helloService = new SimpleHelloService();
         String result = helloService.sayHello("Test");
